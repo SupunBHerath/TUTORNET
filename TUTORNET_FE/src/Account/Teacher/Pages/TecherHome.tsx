d62ee4853233943635589dec5cf408fddba54cf8
akhila <akhilalakshan@gmail.com>
2024-05-21 10:55:54 +0530

    Edit Profile

--- TUTORNET_FE/src/Account/Teacher/Pages/TecherHome.tsx
+++ TUTORNET_FE/src/Account/Teacher/Pages/TecherHome.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Navi_Bar from '../Components/Navi_Bar/Navi_Bar'
-import Profile from '../Components/Profile'
+
 import ColorTabs from '../Components/Tabs/Tabs'
 import Introduction from '../Components/Intro/Intro'
+import Profile from '../Components/Profile'
 
 
 const TecherHome = () => {
   return (
     <div>
       <Navi_Bar />
-      <Profile />
+      <Profile/>
       <br /><br />
 
       <div className="container  mt-5" style={{ width: "1000px", justifyContent: "start" }}>
