fea464123520a538b7a64a15d2dd2a2ac6233152
Yasiru Werasingha <163088530+Yasiru142@users.noreply.github.com>
2024-06-04 22:12:05 +0530

    create auth.js file
    this is my auth.js file.

---
+++ TUTORNET_BE/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).json({ message: 'Invalid token.' });
+  }
+};
+
+module.exports = auth;
